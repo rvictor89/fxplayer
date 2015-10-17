a825c7ee3f85d1586c33f2d03a8e04a2f67b2c4b
Ramon Victor <ramon.victor@t-online.de>
2015-10-17 15:38:15 +0200

    Play Button hinzugefügt und entsprechende Logik um einen festgelegten Song zu starten.

--- src/main/java/de/victorfx/fxplayer/controller/FXPlayercontroller.java
+++ src/main/java/de/victorfx/fxplayer/controller/FXPlayercontroller.java
@@ -1,7 +1,26 @@
 package de.victorfx.fxplayer.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
 /**
  * Created by Ramon Victor on 17.10.2015.
  */
 public class FXPlayercontroller {
+
+    public static final String SONGPATH = "D:/Eigene Musik/Test.mp3";
+
+    public Button btnPlay;
+    private Media media;
+    private MediaPlayer mediaplayer;
+
+    public void play(ActionEvent event) {
+        media = new Media(new File(SONGPATH).toURI().toString());
+        mediaplayer = new MediaPlayer(media);
+        mediaplayer.setAutoPlay(true);
+    }
 }
