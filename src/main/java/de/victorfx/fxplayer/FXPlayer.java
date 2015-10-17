57ba05256f3f4043eaa42d88bcbebfb96f01dad7
Ramon Victor <ramon.victor@t-online.de>
2015-10-17 15:04:03 +0200

    Eine leere Scene kann gestartet werden.

--- src/main/java/de/victorfx/fxplayer/FXPlayer.java
+++ src/main/java/de/victorfx/fxplayer/FXPlayer.java
@@ -1,14 +1,25 @@
 package de.victorfx.fxplayer;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
  * Created by Ramon Victor on 17.10.2015.
  */
 public class FXPlayer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent fxplayer = FXMLLoader.load(getClass().getResource("fxml/fxplayer.fxml"));
+        Scene root = new Scene(fxplayer);
+        primaryStage.setScene(root);
+        primaryStage.show();
     }
 }
