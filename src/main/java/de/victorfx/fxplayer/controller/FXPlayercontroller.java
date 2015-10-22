cfddf17da23f473f2ea8d79ec8d906fe1ba30229
Ramon Victor <ramon.victor@t-online.de>
2015-10-22 18:49:21 +0200

    Es wurde ein Slider hinzugefügt, welcher den aktuellen Zeitpunkt sowie die Möglichkeit bietet den Song durchzuspulen.

--- src/main/java/de/victorfx/fxplayer/controller/FXPlayercontroller.java
+++ src/main/java/de/victorfx/fxplayer/controller/FXPlayercontroller.java
@@ -6,9 +6,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.FileChooser;
+import javafx.util.Duration;
 
 import java.io.File;
 
@@ -17,6 +19,8 @@ import java.io.File;
  */
 public class FXPlayercontroller {
 
+    @FXML
+    private Slider sliderTime;
     @FXML
     private Button btnStop;
     @FXML
@@ -27,20 +31,16 @@ public class FXPlayercontroller {
     private MediaPlayer mediaplayer;
     private FileChooser fc;
     private String songpath;
+    private Boolean isSliderPressed = false;
+
 
     public void play(ActionEvent event) {
         if (mediaplayer == null) {
             media = new Media(new File(songpath).toURI().toString());
             mediaplayer = new MediaPlayer(media);
-            mediaplayer.currentTimeProperty().addListener(new InvalidationListener() {
-                public void invalidated(Observable observable) {
-                    int minutes = (int) mediaplayer.getCurrentTime().toMinutes() % 60;
-                    int seconds = (int) mediaplayer.getCurrentTime().toSeconds() % 60;
-                    int minutesDuration = (int) mediaplayer.getTotalDuration().toMinutes() % 60;
-                    int secondsDuration = (int) mediaplayer.getTotalDuration().toSeconds() % 60;
-                    timelabel.setText(String.format("%02d:%02d / %02d:%02d", minutes, seconds, minutesDuration, secondsDuration));
-                }
-            });
+            mediaplayer.currentTimeProperty().addListener(new timelabelListener());
+            sliderTime.valueProperty().addListener(new sliderTimeListener());
+            sliderTime.setDisable(false);
             mediaplayer.setAutoPlay(true);
             btnPlay.setText("Pause");
             btnStop.setDisable(false);
@@ -81,4 +81,31 @@ public class FXPlayercontroller {
             btnPlay.setText("Play");
         }
     }
+
+    private class sliderTimeListener implements InvalidationListener {
+        public void invalidated(Observable observable) {
+            if (sliderTime.isPressed()) {
+                isSliderPressed = true;
+                mediaplayer.pause();
+                mediaplayer.seek(new Duration(sliderTime.getValue() * 1000));
+                mediaplayer.play();
+                isSliderPressed = false;
+            }
+        }
+    }
+
+    private class timelabelListener implements InvalidationListener {
+        public void invalidated(Observable observable) {
+            int minutes = (int) mediaplayer.getCurrentTime().toMinutes() % 60;
+            int seconds = (int) mediaplayer.getCurrentTime().toSeconds() % 60;
+            int minutesDuration = (int) mediaplayer.getTotalDuration().toMinutes() % 60;
+            int secondsDuration = (int) mediaplayer.getTotalDuration().toSeconds() % 60;
+            timelabel.setText(String.format("%02d:%02d / %02d:%02d", minutes, seconds, minutesDuration, secondsDuration));
+            if (!isSliderPressed) {
+                sliderTime.setMax(mediaplayer.getTotalDuration().toSeconds());
+                sliderTime.setValue(mediaplayer.getCurrentTime().toSeconds());
+            }
+        }
+    }
+
 }
